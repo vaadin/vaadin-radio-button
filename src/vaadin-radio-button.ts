8a7574b32e5c8c5821f7cc533a3efd25eae0bc42
web-padawan <iamkulykov@gmail.com>
2020-03-09 16:29:16 +0200

    chore: create API documentation

--- src/vaadin-radio-button.ts
+++ src/vaadin-radio-button.ts
@@ -4,6 +4,20 @@ import { RadioButtonMixin } from './vaadin-radio-button-mixin';
 
 /**
  * `<vaadin-radio-button>` is a Web Component for radio buttons.
+ *
+ * @attr label-empty - Set when there is no label provided.
+ * @attr disabled - Set when the radio button is disabled.
+ * @attr focus-ring - Set when the radio button is focused using the keyboard.
+ * @attr focused - Set when the radio button is focused.
+ * @attr checked - Set when the radio button is checked.
+ *
+ * @slot - Slot for the label content.
+ *
+ * @csspart radio - The radio button element.
+ * @csspart label - The label content element.
+ *
+ * @event change - Fired when the user toggles the radio button.
+ * @event checked-changed - Fired when the `checked` property changes.
  */
 @customElement('vaadin-radio-button')
 export class VaadinRadioButton extends RadioButtonMixin(RadioButtonBase) {
---
+++ vaadin-radio-group.ts
@@ -0,0 +1,3 @@
+import './theme/lumo/vaadin-radio-group';
+
+export { VaadinRadioGroup } from './src/vaadin-radio-group';
