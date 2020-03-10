ca73349aa521211061e0d391859da83e78dfc342
web-padawan <iamkulykov@gmail.com>
2020-03-10 12:52:52 +0200

    refactor: address review feedback

--- src/vaadin-radio-group-base.ts
+++ src/vaadin-radio-group-base.ts
@@ -23,10 +23,6 @@ export class RadioGroupBase extends VaadinElement {
         flex-direction: column;
       }
 
-      :host([label-empty]) [part='label'] {
-        display: none;
-      }
-
       :host(:not([orientation='horizontal'])) [part='group-field'] {
         display: flex;
         flex-direction: column;
