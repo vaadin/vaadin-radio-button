35f3fc477685b96d8ca89c2e572ba354a8cd958a
web-padawan <iamkulykov@gmail.com>
2020-03-27 10:10:44 +0200

    test: use hermione instead of gemini

--- test/visual/test.js
+++ test/visual/test.js
@@ -1,46 +1,40 @@
-gemini.suite('vaadin-radio-button', rootSuite => {
-  function wait(actions, find) {
-    return actions.waitForJSCondition(window => {
-      return window.webComponentsAreReady;
-    }, 80000);
-  }
-
-  rootSuite.before(wait);
+describe('vaadin-radio-button', () => {
+  const locator = '#radio-tests[data-ready]';
 
   ['lumo', 'material'].forEach(theme => {
-    gemini.suite(`radio-button-tests-${theme}`, suite => {
-      suite
-        .setUrl(`radio-button.html?theme=${theme}`)
-        .setCaptureElements('#default-tests')
-        .capture('default')
-        .capture('rtl', actions => {
-          actions.executeJS(window => {
-            window.document.documentElement.setAttribute('dir', 'rtl');
-          });
-        });
+    it(`${theme}-radio-button-default`, function() {
+      return this.browser
+        .url(`radio-button.html?theme=${theme}`)
+        .waitForVisible(locator, 15000)
+        .assertView(`${theme}-radio-button-default`, locator);
     });
 
-    gemini.suite(`radio-group-tests-${theme}`, suite => {
-      suite
-        .setUrl(`radio-group.html?theme=${theme}`)
-        .setCaptureElements('#default-tests')
-        .capture('default', actions => {
-          actions.executeJS(window => {
-            window.focusRadio();
-          });
-        })
-        .capture('rtl', actions => {
-          actions.executeJS(window => {
-            window.document.documentElement.setAttribute('dir', 'rtl');
-          });
-        });
+    it(`${theme}-radio-button-rtl`, function() {
+      return this.browser
+        .url(`radio-button.html?theme=${theme}&dir=rtl`)
+        .waitForVisible(locator, 15000)
+        .assertView(`${theme}-radio-button-rtl`, locator);
     });
 
-    gemini.suite(`radio-group-wrapping-tests-${theme}`, suite => {
-      suite
-        .setUrl(`radio-group-wrapping.html?theme=${theme}`)
-        .setCaptureElements('#wrapping-tests')
-        .capture('wrapping');
+    it(`${theme}-radio-group-default`, function() {
+      return this.browser
+        .url(`radio-group.html?theme=${theme}`)
+        .waitForVisible(locator, 15000)
+        .assertView(`${theme}-radio-group-default`, locator);
+    });
+
+    it(`${theme}-radio-group-rtl`, function() {
+      return this.browser
+        .url(`radio-group.html?theme=${theme}&dir=rtl`)
+        .waitForVisible(locator, 15000)
+        .assertView(`${theme}-radio-group-rtl`, locator);
+    });
+
+    it(`${theme}-radio-group-wrapping`, function() {
+      return this.browser
+        .url(`radio-group-wrapping.html?theme=${theme}`)
+        .waitForVisible(locator, 15000)
+        .assertView(`${theme}-radio-group-wrapping`, locator);
     });
   });
 });
